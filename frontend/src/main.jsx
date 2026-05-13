8da2c893251c668f6b625a58afa16045e622e1ea
mucagreisson <muca.greisson@polotecnologicoimperiese.it>
2026-05-13 12:03:19 +0200

    Clarify comment on React root initialization
    Updated comment for clarity on React root initialization.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-// Questa è la riga 6 che sta dando errore
+// Inizializzazione della root di React con StrictMode
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
